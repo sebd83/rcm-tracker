7617506e01db71bde651e09f7a75f9deb5cc24cd
sebd83 <49837513+sebd83@users.noreply.github.com>
2020-06-27 16:55:33 -0400

    created two new files for inky and pi-boot loop

---
+++ inky_rcm_pass.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# PYTHON 3 code
+
+# Inky pHAT tutorial
+# https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-inky-phat
+import PIL
+from PIL import Image, ImageFont, ImageDraw
