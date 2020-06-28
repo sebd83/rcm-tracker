e61b08d277a994fc1c3ee71aa0fcd876378e2fc9
sebd83 <49837513+sebd83@users.noreply.github.com>
2020-06-27 20:26:37 -0400

    Update test_inky.py
    testing adding a 4th line of text

--- test_inky.py
+++ test_inky.py
@@ -19,6 +19,8 @@ inky_display.set_border(inky_display.BLACK)
 message = "↗ 06:18:06"
 message2 = "⤒ 19.0°"
 message3 = "↘ 06:29:23"
+message4 = "testing 4th line"
+message5 = "testing 5th line"
 messageSAT = "RCM-2"
 
 font = ImageFont.truetype("Verdana.ttf",14)
@@ -26,28 +28,28 @@ fontSAT = ImageFont.truetype("Verdana Bold.ttf",12)
 w1, h1 = font.getsize(message)
 w2, h2 = font.getsize(message2)
 w3, h3 = font.getsize(message3)
+w4, h4 = font.getsize(message4)
+w5, h5 = font.getsize(message5)
 wSAT, hSAT = fontSAT.getsize(messageSAT)
 
 x1 = 212-10-w1
 x2 = 212-10-w2
 x3 = 212-10-w3#center:(212/2) - (w3/2)
+x4 = 212-10-w4
+x5 = 212-10-w5
 xSAT = 20
 ySAT = 104-hSAT-10
 
-print(h1)
-print(h2)
-print(h3)
-ys = (104-h1-h2-h3)/4
+ys = (104-h1-h2-h3-h4)/5
 y1 = ys
-print(y1)
 y2 = 2*ys+h1
-print(y2)
 y3 = 3*ys+h1+h2
-print(y3)
+y4 = 4*ys+h1+h2+h3
 
 draw.text((x1,y1), message, font=font, fill=2) #BLACK=0
 draw.text((x2,y2), message2, font=font, fill=2) #WHITE=1
 draw.text((x3,y3), message3, font=font, fill=2) #YELLOW=2
+draw.text((x4,y4), message4, font=font, fill=2) #YELLOW=2
 draw.text((xSAT,ySAT), messageSAT, font=fontSAT, fill=1)
 #img.save('/Users/sebastien/Desktop/tst.png')
 img.save('./tst.png')
